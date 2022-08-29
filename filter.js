d657259b20ba9388f2b4ee523dd52f98f8655b5d
unknown <spprince007@gmail.com>
2022-08-29 10:36:50 +0600

    small of number

--- filter.js
+++ filter.js
@@ -1,3 +1,4 @@
 const numbers=[32, 40, 19, 22, 12, 15, 09, 50];
-const number=numbers.filter(number=> number > 20);
-console.log(number);
+//const number=numbers.filter(number=> number > 20);
+const smallNum=numbers.filter(number=> number<20);
+console.log(smallNum);
