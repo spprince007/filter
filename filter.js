c1a93107a0c46cbee40c7f98b71a5a73f5594e0c
unknown <spprince007@gmail.com>
2022-08-29 11:01:49 +0600

    odd numer

--- filter.js
+++ filter.js
@@ -1,4 +1,10 @@
 const numbers=[32, 40, 19, 22, 12, 15, 09, 50];
 //const number=numbers.filter(number=> number > 20);
 const smallNum=numbers.filter(number=> number<20);
+const verySmall=numbers.filter(n=> n<10);
+const evenNumber=numbers.filter(n=> n%2===0);
+const oddNumber=numbers.filter(n=> n%2 != 0);
 console.log(smallNum);
+console.log(verySmall);
+console.log(evenNumber);
+console.log(oddNumber);
